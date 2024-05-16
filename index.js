a365fec6e772ec56fbbe59a48fe3bb25797558c6
braviki92821 <braviki92821@gmail.com>
2024-05-16 00:04:10 -0600

    archivos de prueba

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ app.use(bodyParser.urlencoded({ extended:true }))
 //app.use(cors())
 
 app.use('/', routes())
+app.use("/uploads", express.static(path.resolve(__dirname, 'uploads')));
 
 app.listen(3000)
